d7d20c3224504bc7b6f279f9840d2d15cea42f24
Harvey Cortes Roa <39845967+Lighting-Sun@users.noreply.github.com>
2024-09-20 02:43:59 -0500

    Folder structure (#1)
    * folder-structure: added folder structure
    
    * folder-structure: added semicolons and eof lines

--- tests/pages/page.js
+++ tests/pages/page.js
@@ -1,4 +1,4 @@
-import { browser } from '@wdio/globals'
+import { browser } from '@wdio/globals';
 
 /**
 * main page object containing all methods, selectors and functionality
@@ -9,7 +9,7 @@ export default class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+    open(path) {
+        return browser.url(`https://the-internet.herokuapp.com/${path}`);
     }
 }
