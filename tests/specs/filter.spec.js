ba3adf50358531357909ceb3d7f27f70e32c748c
Harvey Cortes Roa <39845967+Lighting-Sun@users.noreply.github.com>
2024-10-02 05:52:11 -0500

    doing this merge because I forgor to switch branches  (#5)
    * Implemented the login with invalid user scenario
    +Modified the data file to have a better encapsulation of users
    +in login.page.js changed the selector for the error message and added a method that returns the text from it
    +Login.spec.js implemented the invalid login scenario
    
    * edited the name of the invalid login scenario
    
    * Implemented successful logout scenario
    
    +Created a sidemenu component with locators and methods for accessing the side menu options
    +Added a method for clicking the burger menu button and added the sidemenu as a memeber for this class
    +login page, added a selector and a method for getting the logo text
    +login spec implemented the logout scenario
    +wdioFactory.utils fixed an issue with the getElementByValue method, now returns the custom selector correctly and renamed the fucntion to getSelectorByValue
    
    * implemented the sort products by price scenario
    +header component, modified the name and selectors for the select dropdown, also added a method for selecting and option from the selector
    +Inventory page, added a method for getting item prices' text and deleting the dollar sign
    +wdioFactoryUtils added a getElements method, that returns multiple elements from the page, added getTextFromElementsMethod that returns the text from multiple elements and added selectOptionFromSelect method that allows for selecting a specific option from a select
    +Added filter.spec with the scenario that succesfully sorts products

---
+++ tests/specs/filter.spec.js
@@ -0,0 +1,29 @@
+import loginPage from "../pages/login.page";
+import inventoryPage from "../pages/inventory.page";
+import UtilsMethods from "../utils/utilsMethods.utils";
+
+//this import is used to read files with data, see placeHolderData.js
+import { readFileSync } from "fs";
+
+
+describe('login related scenarios', () => {
+
+    beforeEach(async () => {
+        await loginPage.openPage();
+    });
+    //reading the datafile
+    const data = JSON.parse(readFileSync('./tests/data/placeHolderData.json', 'utf-8'));
+    it('Should successfuly sort products', async () => {
+        //TODO the test case is completed, reporting logs are missing
+        await loginPage.fillUsername(data.users.validUser.username);
+        await loginPage.fillPassword(data.users.validUser.password);
+        await loginPage.clicklOnLoginBtn();
+        await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
+        await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
+        const beforeSortingPrices = UtilsMethods.sortLowToHighValues((await inventoryPage.getTextFromPrices()));
+        await inventoryPage.header.clickOnSortFilterDropdownOption('lohi');
+        const afterSortingPrices = await inventoryPage.getTextFromPrices();
+        await expect(beforeSortingPrices).toEqual(afterSortingPrices);
+    });
+
+});
