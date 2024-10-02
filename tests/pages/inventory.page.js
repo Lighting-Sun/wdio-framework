0cb2085091a906f383b94f0492bf921db5f0ffbf
Harvey C <harveydavid14@gmail.com>
2024-10-02 12:21:50 -0500

    +Inventory page, added locators for getting item price, name and add to cart button based on an index value +Implemented the respective methods for getting the price and name of an item based on an index +Implemented a method for clicking in a add to cart button based on an index +Implemented a method that clicks on a add to cart button and also returns the details from the item that was clicked on +Implemented a method that returns the get number of items from the page +Created addProductsToCart.Spec.js

--- tests/pages/inventory.page.js
+++ tests/pages/inventory.page.js
@@ -16,6 +16,22 @@ class Inventory extends Page {
         addToCartButtonBasedOnItemName: {
             selector: "//div[text()='${value}']/ancestor-or-self::div[@class='inventory_item_description']//button[text()='Add to cart']",
             description: "add to cart button based on item name '${value}'",
+        },
+        inventoryItemPriceIndex: {
+            selector: "div[data-test='inventory-item']:nth-of-type(${value}) div[class='inventory_item_price']",
+            description: "inventory item price based on index ${value}'",
+        },
+        inventoryItemNameIndex: {
+            selector: "div[data-test='inventory-item']:nth-of-type(${value}) div[class='inventory_item_name']",
+            description: "inventory item name based on index ${value}'",
+        },
+        inventoryAddCartItemButtonIndex: {
+            selector: "div[data-test='inventory-item']:nth-of-type(${value}) button[class*='btn_small']",
+            description: "inventory item button based on index ${value}'",
+        },
+        inventoryItemCard: {
+            selector: "div[data-test='inventory-item']",
+            description: "inventory item name based on index'",
         }
     };
 
@@ -32,6 +48,32 @@ class Inventory extends Page {
         const textFromPrices = await this.wdioFactory.getTextFromElements(this.locators.inventoryItemPrice);
         const trimmedPrice = textFromPrices.map(textToTrim => textToTrim.slice(1));
         return trimmedPrice;
+    }
+
+    async getInventoryPriceFromIndex(index) {
+        return await this.wdioFactory.getText(await this.wdioFactory.getSelectorByValue(this.locators.inventoryItemPriceIndex, index));
+    }
+
+    async getInventoryNameFromIndex(index) {
+        return await this.wdioFactory.getText(await this.wdioFactory.getSelectorByValue(this.locators.inventoryItemNameIndex, index));
+    }
+
+    async clickAddCartItemButtonFromIndex(index) {
+        return await this.wdioFactory.click(await this.wdioFactory.getSelectorByValue(this.locators.inventoryAddCartItemButtonIndex, index));
+    }
+
+    async clickAndGetDetailsFromItemIndex(index) {
+        const itemNameText = await this.getInventoryNameFromIndex(index);
+        const itemPriceText = await this.getInventoryPriceFromIndex(index);
+        await this.clickAddCartItemButtonFromIndex(index);
+        return {
+            itemName: itemNameText,
+            itemPrice: itemPriceText,
+        };
+    }
+
+    async getNumberOfItems() {
+        return (await this.wdioFactory.getElements(this.locators.inventoryItemCard)).length;
     }
 }
 export default new Inventory();
---
+++ tests/specs/addProductsToCart.spec.js
@@ -0,0 +1,25 @@
+import loginPage from "../pages/login.page";
+import inventoryPage from "../pages/inventory.page";
+import UtilsMethods from "../utils/utilsMethods.utils";
+
+//this import is used to read files with data, see placeHolderData.js
+import { readFileSync } from "fs";
+
+
+describe('product pruchase scenarios', () => {
+
+    beforeEach(async () => {
+        await loginPage.openPage();
+    });
+    //reading the datafile
+    const data = JSON.parse(readFileSync('./tests/data/placeHolderData.json', 'utf-8'));
+    it('Should add and validate multiple items added to cart', async () => {
+        //TODO the test case is completed, reporting logs are missing
+        await loginPage.fillUsername(data.users.validUser.username);
+        await loginPage.fillPassword(data.users.validUser.password);
+        await loginPage.clicklOnLoginBtn();
+        await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
+        await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
+    });
+
+});
