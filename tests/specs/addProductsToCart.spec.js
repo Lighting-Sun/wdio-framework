d92858c01781fbbe66f462691db266ddc68c6d26
Harvey Cortes Roa <39845967+Lighting-Sun@users.noreply.github.com>
2024-10-03 19:44:27 -0500

    Login refactor (#10)
    * Added log in with credentials method
    
    * Added the new method as a step to test cases that are not focused on login functionality
    
    * Added the new method to log in in completePurchase.spec
    
    * fixed a bug where the number returned when reducing an awway had multiple decimal places
    
    * installed allure reporter

--- tests/specs/addProductsToCart.spec.js
+++ tests/specs/addProductsToCart.spec.js
@@ -15,9 +15,7 @@ describe('product pruchase scenarios', () => {
     const data = JSON.parse(readFileSync('./tests/data/placeHolderData.json', 'utf-8'));
     it('Should add and validate multiple items added to cart', async () => {
         //TODO the test case is completed, reporting logs are missing
-        await loginPage.fillUsername(data.users.validUser.username);
-        await loginPage.fillPassword(data.users.validUser.password);
-        await loginPage.clicklOnLoginBtn();
+        await loginPage.loginWithCredentials(data.users.validUser.username, data.users.validUser.password);
         await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
         await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
         const result = await inventoryPage.addRandomItemsToCart();
@@ -36,9 +34,7 @@ describe('product pruchase scenarios', () => {
 
     it('Should add and validate a single specific item to cart', async () => {
         //TODO the test case is completed, reporting logs are missing
-        await loginPage.fillUsername(data.users.validUser.username);
-        await loginPage.fillPassword(data.users.validUser.password);
-        await loginPage.clicklOnLoginBtn();
+        await loginPage.loginWithCredentials(data.users.validUser.username, data.users.validUser.password);
         await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
         await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
         const result = await inventoryPage.AddItemToCartByName('Sauce Labs Onesie');
--- tests/specs/completePurchase.spec.js
+++ tests/specs/completePurchase.spec.js
@@ -18,9 +18,7 @@ describe('complete purchase scenarios', () => {
     const data = JSON.parse(readFileSync('./tests/data/placeHolderData.json', 'utf-8'));
     it('Should do a successful purchase', async () => {
         //TODO the test case is completed, reporting logs are missing
-        await loginPage.fillUsername(data.users.validUser.username);
-        await loginPage.fillPassword(data.users.validUser.password);
-        await loginPage.clicklOnLoginBtn();
+        await loginPage.loginWithCredentials(data.users.validUser.username, data.users.validUser.password);
         await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
         await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
         const result = await inventoryPage.addRandomItemsToCart();
