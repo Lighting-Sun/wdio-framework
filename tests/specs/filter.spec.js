0dc423f623b3d03079d3bc28b6e1f896a693c943
Harvey C <harveydavid14@gmail.com>
2024-10-03 19:20:01 -0500

    Added the new method as a step to test cases that are not focused on login functionality

--- tests/specs/filter.spec.js
+++ tests/specs/filter.spec.js
@@ -15,9 +15,7 @@ describe('login related scenarios', () => {
     const data = JSON.parse(readFileSync('./tests/data/placeHolderData.json', 'utf-8'));
     it('Should successfuly sort products', async () => {
         //TODO the test case is completed, reporting logs are missing
-        await loginPage.fillUsername(data.users.validUser.username);
-        await loginPage.fillPassword(data.users.validUser.password);
-        await loginPage.clicklOnLoginBtn();
+        await loginPage.loginWithCredentials(data.users.validUser.username, data.users.validUser.password);
         await expect(browser).toHaveUrl(expect.stringContaining('/inventory'));
         await expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
         const beforeSortingPrices = UtilsMethods.sortLowToHighValues((await inventoryPage.getTextFromPrices()));
